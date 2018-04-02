c2e6e1d87aea06b2d0df292cfa872beaed026b8f
Shashi Singh <shashi.we@gmail.com>
2018-04-02 15:18:37 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
-
+import { UsersService } from './users/users.service';
+import { UserNewComponent } from './users/user-new/user-new.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserListComponent,
+    UserNewComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
